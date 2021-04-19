75f76ab3dcd7f3e7f7496f2e1108313a6198d40b
dikoleshubh <dikole.shubh@gmail.com>
2021-04-19 23:36:41 +0530

    EMPLOYEE WAGE CALCULATION THROUGH INTERFACE METHOD

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Empwage_Interface
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            EmployeeBuilder employeeBuilder = new EmployeeBuilder();   //Create object of class
+            employeeBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);  //call method
+            employeeBuilder.addCompanyEmpWage("Reliance", 10, 4, 20); //call method
+            employeeBuilder.ComputeEmpWage();  //call method
+            Console.WriteLine("Totsl Wage For Dmart Comapny :- " + employeeBuilder.getTotalWage("Dmart"));
+        }
+    }
+}
