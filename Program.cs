06f3521de6f8cc9d64fdd5d53c73eea7d8ec8ef8
dikoleshubh <79848187+dikoleshubh@users.noreply.github.com>
2021-04-20 18:36:45 +0530

    Employee Attendece

--- Program.cs
+++ Program.cs
@@ -1,6 +1,62 @@
 ﻿using System;
 
 namespace Empwage_Interface
+      
+
+    class EmployeeAttendence
+    {
+        public static void employee() ////Employee Attendece Cases
+        {
+            const int present = 1;
+            Random rnd = new Random();
+            int num = rnd.Next(0,2);
+            if (num == present)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Dailywage(num);
+            
+            
+            
+        }
+         /// Emplloyee Wage System
+        /// <param name="num"></param>
+        private static void Dailywage(int num)  
+        {
+            int empwage;
+            int partempwage;
+            int hr = 8;
+            if(num == 1)
+            {
+                empwage = hr * 20;
+                Console.WriteLine("Daily Employee Wage-{0}", +empwage);
+                
+            }
+            else
+            {
+                Console.WriteLine("No wage");
+            }
+            if (num == 1)
+            {
+                partempwage = hr * 20;
+                Console.WriteLine("Part Employee Wage-{0}", +partempwage);
+
+            }
+            else
+            {
+                Console.WriteLine("No wage");
+            }
+           
+
+        }
+
+    
+}
 {
     class Program
     {
