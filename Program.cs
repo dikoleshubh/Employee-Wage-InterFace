5f96592e30a470f9293b10772484789f07064765
dikoleshubh <79848187+dikoleshubh@users.noreply.github.com>
2021-04-20 18:48:13 +0530

    Attendance Calculation of the  employee

--- Program.cs
+++ Program.cs
@@ -1,16 +1,64 @@
-﻿using System;
+using System;
+using System.Collections.Generic;﻿
+
 
 namespace Empwage_Interface
+    
+    
 {
-    class Program
-    {
-        public static void Main(String[] args)
+    class EmployeeAttendence
+    {/// <summary>
+    /// 
+    /// </summary>
+        public static void employee()
+        { ////Attendece function using Random Variable
+            const int present = 1;
+            Random rnd = new Random();
+            int num = rnd.Next(0,2);
+            if (num == present)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Dailywage(num);
+            
+        }/// <summary>
+        /// /cart time & Daily Employee
+        /// </summary>
+        /// <param name="num"></param>
+        private static void Dailywage(int num)
         {
-            EmployeeBuilder employeeBuilder = new EmployeeBuilder();   //Create object of class
-            employeeBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);  //call method
-            employeeBuilder.addCompanyEmpWage("Reliance", 10, 4, 20); //call method
-            employeeBuilder.ComputeEmpWage();  //call method
-            Console.WriteLine("Totsl Wage For Dmart Comapny :- " + employeeBuilder.getTotalWage("Dmart"));
+            int empwage;
+            int partempwage;
+            int hr = 8;
+            if(num == 1)
+            {
+                empwage = hr * 20;
+                Console.WriteLine("Daily Employee Wage-{0}", +empwage);
+                
+            }
+            else
+            {
+                Console.WriteLine("No wage");
+            }
+            if (num == 1)
+            {
+                partempwage = hr * 20;
+                Console.WriteLine("Part Employee Wage-{0}", +partempwage);
+
+            }
+            else
+            {
+                Console.WriteLine("No wage");
+            }
+           
+
         }
+
     }
+   
 }
